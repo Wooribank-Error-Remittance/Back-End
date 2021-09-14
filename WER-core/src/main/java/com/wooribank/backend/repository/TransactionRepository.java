507caa4907a22b0a6a8a0bc1b84a62e66011266f
김승우 <seungwookim.dev@gmail.com>
2021-09-14 17:50:06 +0900

    [Add] 리포지토리 생성

---
+++ WER-core/src/main/java/com/wooribank/backend/repository/TransactionRepository.java
@@ -0,0 +1,6 @@
+package com.wooribank.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<TransactionRepository, Long> {
+}
