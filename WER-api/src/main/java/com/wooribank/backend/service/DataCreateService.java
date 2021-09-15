d2b58b6603db2fa88327fc9ef5217d7c7aeb5ca1
김승우 <seungwookim.dev@gmail.com>
2021-09-16 03:05:04 +0900

    [Fix] 계좌 정보 수정

--- WER-api/src/main/java/com/wooribank/backend/service/DataCreateService.java
+++ WER-api/src/main/java/com/wooribank/backend/service/DataCreateService.java
@@ -39,17 +39,17 @@ public class DataCreateService {
         bankRepository.save(bank1);
         bankRepository.save(bank2);
 
-        Account account1 = new Account("1002374912345", "WON 통장", 3000000, wooriUser1, bank1);
-        Account account2 = new Account("1002456812345", "스무살우리 통장", 650000, wooriUser1, bank1);
-        Account account3 = new Account("1002313612345", "위비모바일 통장", 54350000, wooriUser1, bank1);
-        Account account4 = new Account("1040540812345", "우리 Magic6 적금", 41000000, wooriUser1, bank1);
-        Account account5 = new Account("1073048412345", "주택청약종합저축", 7700000, wooriUser1, bank1);
+        Account account1 = new Account("1002-374-912345", "WON 통장", 3000000, wooriUser1, bank1);
+        Account account2 = new Account("1002-456-812345", "스무살우리 통장", 650000, wooriUser1, bank1);
+        Account account3 = new Account("1002-313-612345", "위비모바일 통장", 54350000, wooriUser1, bank1);
+        Account account4 = new Account("1040-540-812345", "우리 Magic6 적금", 41000000, wooriUser1, bank1);
+        Account account5 = new Account("1073-048-412345", "주택청약종합저축", 7700000, wooriUser1, bank1);
 
-        Account account6 = new Account("1002374967890", "WON 통장", 4200000, wooriUser2, bank1);
-        Account account7 = new Account("1002456867890", "스무살우리 통장", 350000, wooriUser2, bank1);
-        Account account8 = new Account("1002313667890", "위비모바일 통장", 672130000, wooriUser2, bank1);
-        Account account9 = new Account("1040540867890", "우리 Magic6 적금", 13000000, wooriUser2, bank1);
-        Account account10 = new Account("1073048467890", "주택청약종합저축", 4900000, wooriUser2, bank1);
+        Account account6 = new Account("1002-374-967890", "WON 통장", 4200000, wooriUser2, bank1);
+        Account account7 = new Account("1002-456-867890", "스무살우리 통장", 350000, wooriUser2, bank1);
+        Account account8 = new Account("1002-313-667890", "위비모바일 통장", 672130000, wooriUser2, bank1);
+        Account account9 = new Account("1040-540-867890", "우리 Magic6 적금", 13000000, wooriUser2, bank1);
+        Account account10 = new Account("1073-048-467890", "주택청약종합저축", 4900000, wooriUser2, bank1);
 
         accountRepository.save(account1);
         accountRepository.save(account2);
